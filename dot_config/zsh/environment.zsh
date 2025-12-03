26afc7eb3b6e6752eb64b4c88877dcb7094f2b05
Hummr <daniel.laber@hummr.me>
2025-12-03 12:01:37 +0100

    "Change structure to chezmoi"

---
+++ dot_config/zsh/environment.zsh
@@ -0,0 +1,12 @@
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
+export ZSH_CACHE="$XDG_CACHE_HOME/zsh"
+export ZSH_THEME="powerlevel10k/powerlevel10k"
+export ZSH_CACHE="$XDG_CACHE_HOME/zsh"
+export STARSHIP_CONFIG="$ZSH_CONFIG/starship/starship.toml"
+export SYSTEMD_EDITOR=nvim
+export OLLAMA_HOST=0.0.0.0
+export SSH_AGENT_PID=""
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
+export GNUPGHOME="$HOME/.config/gnupg"
