bed1a0b15bccac00a6871ac4ecaed8f58e65a7ad
Hummr <daniellaber@hummr.me>
2025-12-07 20:34:42 +0100

    Add latex support

--- dot_config/nvim/lua/user/keymaps.lua
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -19,7 +19,6 @@ map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]ui
 -- Exit terminal mode
 map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
-
 -- [[ Basic Autocommands ]]
 -- Highlight when yanking (copying) text
 au("TextYankPost", {
@@ -29,3 +28,19 @@ au("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+-- LaTeX / VimTeX Keymaps
+-- Compile (Build) and View PDF
+map("n", "<leader>ll", "<cmd>VimtexCompile<cr>", { desc = "VimTeX: [L]atex [L]aunch Compile" })
+map("n", "<leader>lv", "<cmd>VimtexView<cr>", { desc = "VimTeX: [L]atex [V]iew PDF" })
+
+-- Stop compilation or toggle continuous compilation
+map("n", "<leader>ls", "<cmd>VimtexStop<cr>", { desc = "VimTeX: [L]atex [S]top Compilation" })
+map("n", "<leader>lt", "<cmd>VimtexToggle<cr>", { desc = "VimTeX: [L]atex [T]oggle Continuous Compilation" })
+
+-- Clean auxiliary files
+map("n", "<leader>lc", "<cmd>VimtexClean<cr>", { desc = "VimTeX: [L]atex [C]lean Aux Files" })
+
+-- Forward Search (Editor to Viewer)
+-- This is usually not required as View/Compile does it, but good for explicit use.
+map("n", "<leader>lf", "<cmd>VimtexView<cr>", { desc = "VimTeX: [L]atex [F]orward Search" })
--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -35,6 +35,7 @@ local function setup_lsp_config()
 	-- Language Servers List
 	local servers = {
 		hyprls = {},
+		texlab = {},
 		lua_ls = {
 			settings = {
 				Lua = {
@@ -91,14 +92,9 @@ local function setup_lsp_config()
 			local client = vim.lsp.get_client_by_id(event.data.client_id)
 			if
 				client
-				and client_supports_method(
-					client,
-					vim.lsp.protocol.Methods.textDocument_documentHighlight,
-					event.buf
-				)
+				and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
 			then
-				local highlight_augroup =
-					vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+				local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 					buffer = event.buf,
 					group = highlight_augroup,
@@ -149,7 +145,9 @@ return require("lazy").setup({
 			require("mini.surround").setup()
 			local statusline = require("mini.statusline")
 			statusline.setup({ use_icons = vim.g.have_nerd_font })
-			statusline.section_location = function() return "%2l:%-2v" end
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
 		end,
 	},
 	{ -- Utility for pending keybinds
@@ -171,7 +169,20 @@ return require("lazy").setup({
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		opts = {
-			ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "hyprlang", "markdown", "markdown_inline", "query", "vim", "vimdoc" },
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"hyprlang",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+			},
 			auto_install = true,
 			highlight = { enable = true, additional_vim_regex_highlighting = { "ruby" } },
 			indent = { enable = true, disable = { "ruby" } },
@@ -184,14 +195,20 @@ return require("lazy").setup({
 			vim.filetype.add({
 				pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 			})
-		end
+		end,
 	},
 	{ -- Fuzzy Finder
 		"nvim-telescope/telescope.nvim",
 		event = "VimEnter",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = function() return vim.fn.executable("make") == 1 end },
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
 			{ "nvim-telescope/telescope-ui-select.nvim" },
 			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 		},
@@ -213,12 +230,39 @@ return require("lazy").setup({
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-			vim.keymap.set("n", "<leader>/", function() builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({ winblend = 10, previewer = false })) end, { desc = "[/] Fuzzily search in current buffer" })
-			vim.keymap.set("n", "<leader>s/", function() builtin.live_grep({ grep_open_files = true, prompt_title = "Live Grep in Open Files" }) end, { desc = "[S]earch [/] in Open Files" })
-			vim.keymap.set("n", "<leader>sn", function() builtin.find_files({ cwd = vim.fn.stdpath("config") }) end, { desc = "[S]earch [N]eovim files" })
+			vim.keymap.set("n", "<leader>/", function()
+				builtin.current_buffer_fuzzy_find(
+					require("telescope.themes").get_dropdown({ winblend = 10, previewer = false })
+				)
+			end, { desc = "[/] Fuzzily search in current buffer" })
+			vim.keymap.set("n", "<leader>s/", function()
+				builtin.live_grep({ grep_open_files = true, prompt_title = "Live Grep in Open Files" })
+			end, { desc = "[S]earch [/] in Open Files" })
+			vim.keymap.set("n", "<leader>sn", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("config") })
+			end, { desc = "[S]earch [N]eovim files" })
 		end,
 	},
+	{
+		"lervag/vimtex",
+		lazy = false,
+		config = function()
+			-- Ensure g:vimtex_view_method is a single string 'zathura'.
+			-- This prevents the "Can only compare List with List" error.
+			vim.g.vimtex_view_method = "zathura"
 
+			-- Use a custom LaTeX compiler command
+			vim.g.vimtex_compiler_method = "latexmk"
+
+			-- Use the default latexmk engine for xelatex (if needed)
+			vim.g.vimtex_compiler_latexmk_engines = {
+				_ = "-xelatex",
+			}
+
+			-- Other settings (retained from your previous config):
+			vim.g.vimtex_quickfix_enabled = 0
+		end,
+	},
 	--------------------------------------------------------
 	-- 3. LSP, COMPLETION, & FORMATTING (Productivity)
 	--------------------------------------------------------
@@ -238,14 +282,27 @@ return require("lazy").setup({
 		event = "VimEnter",
 		version = "1.*",
 		dependencies = {
-			{ "L3MON4D3/LuaSnip", version = "2.*", build = (function() if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then return end return "make install_jsregexp" end)(), opts = {} },
+			{
+				"L3MON4D3/LuaSnip",
+				version = "2.*",
+				build = (function()
+					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+						return
+					end
+					return "make install_jsregexp"
+				end)(),
+				opts = {},
+			},
 			"folke/lazydev.nvim",
 		},
 		opts = {
 			keymap = { preset = "default" },
 			appearance = { nerd_font_variant = "mono" },
 			completion = { documentation = { auto_show = false, auto_show_delay_ms = 500 } },
-			sources = { default = { "lsp", "path", "snippets", "lazydev" }, providers = { lazydev = { module = "lazydev.integrations.blink", score_offset = 100 } } },
+			sources = {
+				default = { "lsp", "path", "snippets", "lazydev" },
+				providers = { lazydev = { module = "lazydev.integrations.blink", score_offset = 100 } },
+			},
 			snippets = { preset = "luasnip" },
 			fuzzy = { implementation = "lua" },
 			signature = { enabled = true },
@@ -255,7 +312,16 @@ return require("lazy").setup({
 		"stevearc/conform.nvim",
 		event = { "BufWritePre" },
 		cmd = { "ConformInfo" },
-		keys = { { "<leader>f", function() require("conform").format({ async = true, lsp_format = "fallback" }) end, mode = "", desc = "[F]ormat buffer" } },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_format = "fallback" })
+				end,
+				mode = "",
+				desc = "[F]ormat buffer",
+			},
+		},
 		opts = {
 			notify_on_error = false,
 			format_on_save = function(bufnr)
@@ -276,7 +342,15 @@ return require("lazy").setup({
 	"NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
 	{ -- Adds git related signs to the gutter
 		"lewis6991/gitsigns.nvim",
-		opts = { signs = { add = { text = "+" }, change = { text = "~" }, delete = { text = "_" }, topdelete = { text = "‾" }, changedelete = { text = "~" } } },
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
 	},
 	{ -- `lazydev` for Lua LSP configuration
 		"folke/lazydev.nvim",
