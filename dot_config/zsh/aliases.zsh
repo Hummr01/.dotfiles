4508fd3c74a514ac54dbf8e819b00231467d8c3e
Hummr <daniellaber@hummr.me>
2026-06-04 20:14:00 +0200

    chore: clean up zsh config

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,46 +1,69 @@
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# 1. Environment Variables & Core Preferences
+# ---------------------------------------------------------------------
 export EDITOR="nvim"
-alias aedit="$EDITOR $ZSH_CONFIG/aliases.zsh; source $ZSH_CONFIG/aliases.zsh"
-alias fedit="$EDITOR $ZSH_CONFIG/functions.zsh; source $ZSH_CONFIG/functions.zsh"
-alias pedit=" $EDITOR $ZSH_CONFIG/private.zsh; source $ZSH_CONFIG/private.zsh"
 
+# ---------------------------------------------------------------------
+# 2. Antidote Plugin Manager (The Optimized Way)
+# ---------------------------------------------------------------------
+# Instead of hardcoding paths for 4 plugins, let Antidote handle it.
+# Ensure your plugins are listed in: ~/.config/zsh/plugins.txt
+# Then, you only need these lines to load everything safely:
+_antidote_dir="${XDG_DATA_HOME:-$HOME/.local/share}/antidote"
+if [[ -d "$_antidote_dir" ]]; then
+    source "$_antidote_dir/antidote.zsh"
+    antidote load "${ZDOTDIR:-$HOME}/plugins.txt"
+fi
+
+# Custom keybinds for plugins
+bindkey '^Y' autosuggest-accept
+
+# ---------------------------------------------------------------------
+# 3. Dotfile & Config Management Aliases
+# ---------------------------------------------------------------------
+# Kept clean using $ZDOTDIR as the source of truth
+alias zedit="$EDITOR ${ZDOTDIR:-$HOME}/.zshrc; source ${ZDOTDIR:-$HOME}/.zshrc"
+alias aedit="$EDITOR ${ZDOTDIR:-$HOME}/aliases.zsh; source ${ZDOTDIR:-$HOME}/aliases.zsh"
+alias fedit="$EDITOR ${ZDOTDIR:-$HOME}/functions.zsh; source ${ZDOTDIR:-$HOME}/functions.zsh"
+alias pedit="$EDITOR ${ZDOTDIR:-$HOME}/private.zsh; source ${ZDOTDIR:-$HOME}/private.zsh"
+alias viedit="$EDITOR $HOME/.vim/vimrc"
+
+# ---------------------------------------------------------------------
+# 4. Applications & Enhancements
+# ---------------------------------------------------------------------
 alias v="nvim"
 alias vi="nvim"
 alias vim="nvim"
 alias gvim="nvim --listen ~/.cache/nvim/godot.pipe ."
-alias aedit="$EDITOR $ZSH_CONFIG/aliases.zsh; source $ZDOTDIR/aliases.zsh"
-alias fedit="$EDITOR $ZSH_CONFIG/functions.zsh; source $ZDOTDIR//functions.zsh"
-alias pedit=" $EDITOR $ZSH_CONFIG/private.zsh; source $ZDOTDIR/private.zsh"
-alias zedit=" $EDITOR $ZSH_CONFIG/.zshrc; source $ZDOTDIR/.zshrc"
-alias viedit=" $EDITOR $HOME/.vim/vimrc"
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
 alias sudo="nocorrect sudo -E "
 
+# Modern CLI replacements
 alias ls="eza --icons --git-ignore"
 alias la="eza --icons -a"
+alias wget='wget --hsts-file="${XDG_DATA_HOME:-$HOME/.local/share}/wget-hsts"'
 
-# Handy change dir shortcuts
+# SSH Escape hatch for Kitty terminal
+alias ssh='kitten ssh'
+
+# ---------------------------------------------------------------------
+# 5. Quality of Life & Navigation Shortcuts
+# ---------------------------------------------------------------------
 alias ..='cd ..'
 alias ...='cd ../..'
 alias .2='cd ../../..'
 alias .3='cd ../../../..'
-alias .4='cd ../../../../..'
+alias .4='cd ../../../../../'
 
-# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 alias mkdir='mkdir -p'
 
-# Fixes "Error opening terminal: xterm-kitty" when using the default kitty term to open some programs through ssh
-alias ssh='kitten ssh'
-
-# Git aliases
+# Git
 alias gb='git branch'
 alias gc='git commit'
 alias ga='git add'
-alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
 
-#Open Project in Zellij session
-alias s='sh $HOME/Scripts/find_and_start_zellij.sh'
-alias f='sh $HOME/Scripts/attach_existing_zellij.sh'
-
-# Wget History
-alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
+# Zellij Session Workflows (Wrapped safely in a check)
+if [[ -d "$HOME/Scripts" ]]; then
+    alias s='sh $HOME/Scripts/find_and_start_zellij.sh'
+    alias f='sh $HOME/Scripts/attach_existing_zellij.sh'
+fi
--- dot_config/zsh/dot_zsh_plugins.zsh
+++
@@ -1,9 +0,0 @@
-fpath+=( $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use )
-source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use/you-should-use.plugin.zsh
-source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use/zsh-you-should-use.plugin.zsh
-fpath+=( $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-auto-notify )
-source $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-auto-notify/auto-notify.plugin.zsh
